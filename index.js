dab3a777fdfc623b4d7e688e0e16d20f34c56597
Jonathan Price <jon.mark.price@gmail.com>
2018-01-22 15:58:27 -0600

    CORS support for RunProgram.

--- index.js
+++ index.js
@@ -4,25 +4,40 @@ const { result } = require('./parser/helpers');
 
 exports.handler = (event, context, callback) => {
   let responseBody = {
-    message: 'No program specified.'
+    result: 'No program specified.'
   };
  
   // Parse body
+  console.log("body:")
+  console.log(JSON.stringify(event.body))
+  
   if (event.body !== null && event.body !== undefined) {
-    const body = JSON.parse(event.body);
-    if (body.program) {
+    const program = JSON.parse(event.body);
+    if (program) {
+      console.log('Program: ');
+      console.log(program);
+      
       // responseBody = { message: 'Running program: ' + body.program + '.' };
-      const output = result(...body.program);
+      const output = result(...program);
       responseBody = {
         result: S.either(S.I, S.compose(display, S.prop('value')), output)
-      }
+      };
     }
   }
+  
+  console.log("Response body: ")
+  console.log(responseBody);
   
   // Send response
   const response = {
     statusCode: 200,
-    body: JSON.stringify(responseBody)
+    body: JSON.stringify(responseBody),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Creditials': true,
+      'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Content-Type': 'application/json'
+    }
   };
   callback(null, response);
 };
