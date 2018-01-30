e446f9d58b44bb4d3c07fc5db2461852e3c6e540
Jonathan Price <jon.mark.price@gmail.com>
2018-01-30 14:24:17 +0900

    Refactor renderPage to use options object instead of 5 parameters.

--- mcilroy/server/pages/login.js
+++ mcilroy/server/pages/login.js
@@ -1,57 +1,19 @@
-// PORT registration here
-// IDEA: try to *import* a function defined elsewhere, e.g. in dist/
-// from webpack...
 'use strict';
-
-// XXX DEPRECATED
-// const { renderToString } = require('react-dom/server');
 
 const React = require('react');
 const renderPage = require('../renderPage');
 const LoginPage = require('../../lib/components/LoginPage');
 
-// TODO: do the same a mcilroy..?
 // McIlroy EB used req.flash and I passed {flash: ...} into 
 // window.__INITIAL_STATE__ just as one would do with Redux.
 
-// TODO: this will need state as well eventually
-const page = React.createElement(LoginPage, null, null);
-// const appHtml = renderToString(page);
-
-/* // Old version
-const html = `
-    <h1>SSR Test</h1>
-    <form>
-        <input type="text" name="test" />
-    </form>`;
-*/
-
-const component = page;
-const styles = ['common', 'banner', 'form', 'login', 'flash'];
+// const component = page;
+const stylesheets = ['common', 'banner', 'form', 'login', 'flash'];
 const state = {flash: null};
-
 module.exports = () => 
-  // TODO use param. object!
-  renderPage(
-    component,          // component
-    'Log In - McIlroy', // title
-    'login',            // css
-    styles,
-    state);
-/*
-module.exports = () => 
-`<!DOCTYPE html>
-<html>
-  <head>
-    <title>McIlroy</title>
-    <link rel="stylesheet" href="https://mcilroy.s3-us-west-1.amazonaws.com/index.css" />
-    
-    <!-- Render these from renderPage -->
-    <!-- <link rel="stylesheet" href="..." /> -->
-  </head>
-  <body>
-    ${appHtml}
-  </body>
-</html>`;
-*/
+  renderPage(React.createElement(LoginPage, state, null), {
+    title: 'Log In - McIlroy',
+    stylesheets,
+    state
+  });
 
--- mcilroy/server/renderPage.js
+++ mcilroy/server/renderPage.js
@@ -1,30 +1,43 @@
 const { renderToString } = require('react-dom/server');
 const { urls } = require('../aws-config');
 const dbg = console.log;
+const PropTypes = require('prop-types');
 // const dbg = require('debug')('routes:helpers');
 
 const stringify = s => JSON.stringify(s).replace(/</g, '\\u003c');
+const optTypes = {
+  title: PropTypes.string,
+  bundles: PropTypes.array,
+  stylesheets: PropTypes.array,
+  state: PropTypes.object
+};
 
-// TODO: Use an arguments object -- 5 arguments is probably too many...
-function renderPage(element, title, bundle = null, stylesheets = [], preloaded = null) {
-  const links = stylesheets.map(
-    name => `<link rel="stylesheet" href=${urls.s3}/styles/${name}.css />`
+function renderPage(element, opts) {
+  // Check prop types
+  PropTypes.checkPropTypes(optTypes, opts, 'option', 'renderPage');
+
+  const title = (opts.title !== undefined) ? opts.title : 'McIlroy';
+
+  const stylesheets = opts.stylesheets || [];
+  const links = stylesheets.map(name => 
+    `<link rel="stylesheet" href="${urls.s3}/styles/${name}.css" />`
   );
+
   const html = renderToString(element);
-  const script = (bundle !== null)
-    ? `<script type="text/javascript" src="${urls.s3}/scripts/${bundle}.bundle.js"></script>`
-    : ''; 
-  const state = (preloaded !== null)
+
+  const bundles = opts.bundles || [];
+  const scripts = bundles.map(name => // need type=text/js?
+    `<script src="${urls.s3}/scripts/${name}.bundle.js"></script>`
+  );
+
+  const preloaded = (opts.state !== undefined)
     ? `<script type="text/javascript">
-        window.__PRELOADED_STATE__ = ${stringify(preloaded)}
+        window.__PRELOADED_STATE__ = ${stringify(opts.state)}
       </script>`
     : '';
 
-  dbg("element %O", element);
-  dbg("rendered: %O", html);
-  
-  dbg('preloaded %o', preloaded);
-  dbg('state %o', state)
+  console.log("INITIAL STATE:");
+  console.log(stringify(opts.state));
 
   return `<!DOCTYPE html>
   <html>
@@ -34,8 +47,7 @@ function renderPage(element, title, bundle = null, stylesheets = [], preloaded =
     </head>
     <body>
       <div id="app">${html}</div>
-      ${state}
-      ${script}
+      ${preloaded} ${scripts.join('\n')}
     </body>
   </html>`;
 }
