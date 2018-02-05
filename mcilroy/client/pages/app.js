0027b21a4b6cc53b543b223c58133615a4da4bb6
Jonathan Price <jon.mark.price@gmail.com>
2018-02-05 12:15:34 +0900

    Add client-side React hydration.

--- mcilroy/client/pages/app.js
+++ mcilroy/client/pages/app.js
@@ -1,1 +1,23 @@
-console.log('dummy client script');
+const React           = require('react');
+const { hydrate }     = require('react-dom');
+const { Provider }    = require('react-redux');
+const Interpretter    = require('../../common/containers/Interpretter');
+const configureStore  = require('../../common/configureStore');
+const dbg = require('../../common/dbgconf')('client');
+const reducer = require('../../common/reducers/appRoot');
+
+dbg('Started successfully!');
+
+const preloadedState = window.__PRELOADED_STATE__;
+delete window.__PRELOADED_STATE__;
+const store = configureStore(reducer, preloadedState);
+
+hydrate( // was render
+  <Provider store={store}>
+    <Interpretter />
+  </Provider>,
+  document.getElementById('app')
+);
+
+// TODO:
+// Now fire off async actions...
