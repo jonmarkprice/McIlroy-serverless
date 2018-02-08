b9f1d088ab0425dc31ed13e72bb0bcddbfba5f51
Jonathan Price <jon.mark.price@gmail.com>
2018-02-08 11:56:57 +0900

    Set and clear cookies from express without express-session.

--- mcilroy/server/session-router.js
+++ mcilroy/server/session-router.js
@@ -1,52 +1,33 @@
 const express = require('express');
-const session = require('express-session');
-const AWS = require('aws-sdk');
-const DynamoStore = require('connect-dynamodb')({session: session});
+// const AWS = require('aws-sdk');
+
+// Use cookie parser instead of express-session
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// AWS.config.region = 'us-east-1'; 
-/*
-const options = {
-  AWSConfigJSON: {
-    region: 'us-east-1',
-    accessKeyId: 'fakeid',
-    secretAccessKey: 'sosecret'
-  },
-  client: new AWS.DynamoDB({
-    endpoint: new AWS.Endpoint('http://localhost:8000')
-  })
-};
-*/
-
-app.use(session({
-  store: new DynamoStore({}),
-  secret: 'keyboard dog',
-  resave: true,             // TODO Lookup
-  saveUninitialized: false, // TODO Lookup
-}));
+app.use(cookieParser()); // no secret needed for now
 
 app.get('/users/home', (req, res) => {
-  if (req.session.known) {
+  if (req.cookies.username === 'monty') {
     res.send('<p>Welcome friend!</p><a href="logout">logout</a>');
   } else {
     res.write('<p>')
     res.write('  Your mother was a newt...');
     res.write('  and your father smelt of elderberries.');
     res.write('</p>');
-    res.write('<a href="login">login</a>');
-    res.end();
+    res.send('<a href="login">login</a>');
   }
 });
 
 app.get('/users/login', (req, res) => {
-  req.session.known = true;
-  res.redirect('/users/home');
+  res.cookie("username", "monty");
+  res.redirect('home');
 });
 
 app.get('/users/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/users/home');
+  res.clearCookie("username");
+  res.redirect('home');
 });
 
 module.exports = app;
